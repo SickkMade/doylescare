2b50cf590a2fceb1c120536de228cd0e11de67dc
sickk <jaakmyr@gmail.com>
2025-02-11 21:41:39 -0800

    doyle done

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useRef } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const playVideo = () => {
+    try {
+        if (videoRef.current) {
+            if (videoRef.current.requestFullscreen) {
+                videoRef.current.requestFullscreen();
+            } 
+            videoRef.current.play();
+        }
+    } catch (error) {
+        console.error('Error playing fullscreen video:', error);
+    }
+};
 
+const handleVideoEnd = () => {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    }
+    window.location.reload();
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <button onClick={playVideo}>Enter Doyle's Website</button>
+    <div className="scare">
+      <video
+          ref={videoRef}
+          id="app--video"
+          preload="auto"
+          playsInline
+          onEnded={handleVideoEnd}>
+          <source src="https://github.com/SickkMade/monkescare/raw/refs/heads/main/public/video1.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+      </video>
+    </div>
+    
     </>
   )
 }
